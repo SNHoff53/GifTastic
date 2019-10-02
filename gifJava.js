e0bb63fad28924ddc51c0aa1d893512bfa17a803
hoffs <stephanie.nhoffman7@gmail.com>
2019-10-02 01:40:54 -0700

    fixed issues with gif pull, and setting up newGifButton

--- gifJava.js
+++ gifJava.js
@@ -6,41 +6,71 @@ var topics = [
     "cats"
 ];
 
-// Event listener for all of our button elements
-$("button").on("click", function(){
 
+function topicsDisplay(){
+    $("#buttons-area").empty();
 // Declaring variable of topics 
-topics = $(this).attr("data-topics");
+    topics = $(this).attr("data-name");
 // constructing a URL to search Gify for a random gif when button is clicked
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=fAd8nhiRNR2VtraFeMkS7t2FaNyqITEc&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=fAd8nhiRNR2VtraFeMkS7t2FaNyqITEc&limit=10";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(queryURL);
 
-    console.log(response);
+        console.log(response);
 
-    var gifResults = response.data;
+        var gifResults = response.data;
 
-    // looping/counting the gifResult items
-    for (var i = 0; i < gifResults.length; i++) {
-            
-            var gifDiv = $("<div>");
+        // looping/counting the gifResult items
+        for (var i = 0; i < gifResults.length; i++) {
+                
+                var gifDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + gifResults[i].rating);
+                var topicsImage = $("<img>");
+                topicsImage.attr("src", gifResults[i].images.fixed_height.url);
 
-            var p = $("<p>").text("Rating: " + gifResults[i].rating);
-            
-            var topicsImage = $("<img>");
-            topicsImage.attr("src", gifResults[i].images.fixed_height.url);
+                // animation goes here
+                gifDiv.append(p);
+                gifDiv.append(topicsImage);
+                
+                $("#gifs-here").prepend(gifDiv);
 
-            gifDiv.append(p);
-            gifDiv.append(topicsImage);
-            
-            $("#gifs-here").prepend(gifDiv);
+        }
+    });
 
+};
+
+// Render new buttons
+function newButton(){
+    $("#buttons-area").empty();
+
+    // Loops through topics
+    for (var i = 0; i < topics.length; i++){
+        var a = $("<button>");
+        // We are adding the class Topic to our new button
+        a.addClass("topic-btn");
+        // Class for attribute
+        a.attr("data-name", topics[i]);
+        // Text for the new button
+        a.text(topics[i]);
+        // Appending/adding the new button to the button div
+        $("#buttons-area").append(a);
     }
+}
+// A topic button has been clicked
+$("#add-gif").on("click", function(event){
+    event.preventDefault();
+
+    var topic = $("#gif-input").val();
+    topics.push(topic);
+    
+    newButton();
 });
 
-});
+$(document).on("click", ".topic", newButton);
+
+newButton();
